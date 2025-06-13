465b5fcca30dedee5d9e3dd5444ef78d95630134
Fabian Betancourt <fabianrbv@gmail.com>
2025-06-13 01:19:58 -0500

    test 6

--- src/js/movieService.js
+++ src/js/movieService.js
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_KEY = '341e682c586005efd50a82dbd9b94a06';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 export async function getTrendingMovies() {
@@ -25,7 +25,7 @@ export async function getMoviesByGenre(genreId) {
   return data.results;
 }
 
-const YT_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+const YT_API_KEY = 'AIzaSyCKeGrxOrJHl7ZVPxYjogTMyYrGQgW33bc';
 export async function getTrailer(movieTitle) {
   const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(movieTitle + " official trailer")}&key=${YT_API_KEY}&maxResults=1`);
   const data = await response.json();
